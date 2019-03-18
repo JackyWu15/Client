75cce20d51f86eb9297e7875d8cdc033297e374f
Jacky <351181429@qq.com>
2019-03-18 20:13:05 +0800

    messenger接收服务端返回数据

--- app/src/main/java/com/hechuangwu/client/MessengerActivity.java
+++ app/src/main/java/com/hechuangwu/client/MessengerActivity.java
@@ -6,10 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 
 import com.hechuangwu.utils.Config;
@@ -19,22 +21,40 @@ import com.hechuangwu.utils.Config;
  * 功能:
  */
 public class MessengerActivity extends Activity {
-    private static final int MSG = 1001;
-    private static final String MSG_DATA="msg_data";
+    private static final int MSG_RECEIVER = 1001;
+    private static final int MSG_REPLY= 1002;
+    private static final String MSG_RECEIVER_DATA="msg_receiver_data";
+    private static final String MSG_REPLY_DATA="msg_reply_data";
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_messenger );
     }
+    private static Messenger mMessenger = new Messenger( new MessengerHandler() );
+    private static class MessengerHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage( msg );
+            switch (msg.what){
+                case MSG_REPLY:
+                    Bundle data = msg.getData();
+                    String msgData = data.getString( MSG_REPLY_DATA );
+                    Log.i( "data", "handleMessage: >>>reply>>>"+msgData );
+                    break;
+            }
+        }
+    }
+
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             Messenger messenger = new Messenger( service );
             Message message = Message.obtain();
-            message.what = MSG;
+            message.what = MSG_RECEIVER;
             Bundle bundle = new Bundle();
-            bundle.putString( MSG_DATA,"hello messenger server" );
+            bundle.putString( MSG_RECEIVER_DATA,"hello messenger server" );
             message.setData( bundle );
+            message.replyTo = mMessenger;
             try {
                 messenger.send( message );
             } catch (RemoteException e) {
